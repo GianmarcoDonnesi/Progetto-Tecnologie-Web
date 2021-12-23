93335d1968cdab3e7b8e2455008344c0639e0a02
GianmarcoDonnesi <gianmarco.donnesi001@studenti.uniparthenope.it>
2021-12-23 22:25:34 +0100

    new

---
+++ Progetto Tec/market.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/annunci')
+def annunci_page():
+    return render_template('annunci.html')
